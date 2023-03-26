8d65e79e67f7f7211836a64269979ac0c8276320
Laurence Cullen <laurencesimoncullen@gmail.com>
2023-03-27 00:05:45 +0100

    fighting to control TextMeshPro...

--- Assets/Scripts/Persistent.cs
+++ Assets/Scripts/Persistent.cs
@@ -1,9 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public class Persistent : MonoBehaviour
 {
     // Player name
     public string playerName = "Player";
+
+    // Name card
+    public TextMeshProUGUI nameCard;
 
     // Initial number of creatures
     public int numCreatures = 10;
@@ -27,11 +31,11 @@ public class Persistent : MonoBehaviour
     public int generations = 1;
 
     // Awake
-    void Awake()
-    {
-        // Dont destroy this object
-        DontDestroyOnLoad(this.gameObject);
-    }
+    // void Awake()
+    // {
+    //     // Dont destroy this object
+    //     DontDestroyOnLoad(this.gameObject);
+    // }
 
     // Start is called before the first frame update
     void Start()
@@ -41,5 +45,14 @@ public class Persistent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    }
+
+    // Update the player name
+    public void UpdatePlayerName(string newName)
+    {
+        playerName = newName;
+
+        // Update the text on the TMPro.TextMeshProUGUI component of nameCard
+        nameCard.text = playerName;
     }
 }
